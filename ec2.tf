f5acf168fb06e676b94cd9e304ec89ba25d37322
pushpahari <chetalapushpa@gmail.com>
2024-09-07 18:26:41 +0100

    modified

--- ec2.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "web" {
-  ami           = "ami-0522ab6e1ddcc7055"  # Directly specify the AMI ID
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "HelloWorld"
-  }
-}
--- vpc.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_vpc" "ganesh" {
-  cidr_block       = "10.0.0.0/16"
-tags = {
-    Name = "ganesh_vpc"
-  }
-}
-    
